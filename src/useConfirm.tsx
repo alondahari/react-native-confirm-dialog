5012a5f8e7263ec21cfbad3fce4bbeb0b44ce36c
alondahari <a.dahari@contactelevation.com>
2020-10-03 18:37:58 -0700

    Add hook

---
+++ src/useConfirm.tsx
@@ -0,0 +1,23 @@
+import {
+  ConfirmConfig,
+  CurrentConfirm,
+} from 'types'
+import { ConfirmContext } from 'index'
+import { useContext } from 'react'
+
+type ConfirmHook = (config: ConfirmConfig) => void
+
+const useConfirm = (): ConfirmHook => {
+  const { setCurrent, ...current } = useContext<CurrentConfirm>(ConfirmContext)
+
+  return (config: ConfirmConfig): void => {
+    setCurrent &&
+      setCurrent({
+        ...current,
+        open: true,
+        ...config,
+      })
+  }
+}
+
+export default useConfirm
