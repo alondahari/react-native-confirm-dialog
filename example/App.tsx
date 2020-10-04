0d503d210c173c411aebbe037eec8c79bf2d5c5d
alondahari <a.dahari@contactelevation.com>
2020-10-03 18:32:09 -0700

    Add example and build

---
+++ example/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Triggers from './Triggers'
+import ConfirmProvider from 'react-native-confirm-dialog'
+
+export default function App() {
+  return (
+    <ConfirmProvider>
+      <View style={styles.container}>
+        <Triggers />
+      </View>
+    </ConfirmProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ example/Triggers.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from 'react-native'
+
+const Triggers = (): JSX.Element => {
+  const handlePress = () => {
+
+  }
+
+  return (
+    <Button onPress={ handlePress } title='Open Confirm' />
+  )
+}
+
+export default Triggers
